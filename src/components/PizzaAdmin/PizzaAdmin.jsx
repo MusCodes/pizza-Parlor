4dbaaa087ea468fbb74005f18e0359b0ef2b3bee
Mustafa <MustafaGino1@yahoo.com>
2023-08-29 13:14:16 -0500

    date and time converted into from unix to date/time.

--- src/components/PizzaAdmin/PizzaAdmin.jsx
+++ src/components/PizzaAdmin/PizzaAdmin.jsx
@@ -2,6 +2,15 @@ import { useDispatch, useSelector } from "react-redux";
 
 export default function PizzaAdmin() {
   const adminList = useSelector((store) => store.adminReducer);
+  
+
+  // functiong to format timestime into readable date and time
+  function formatDate(timestamp){
+    const createdAt = new Date(timestamp)
+    const time = createdAt.toLocaleTimeString();
+    const date= createdAt.toLocaleDateString();
+    return `${date} ${time}`;
+  }
   return (
     <>
       <div className="order-list">
@@ -19,7 +28,7 @@ export default function PizzaAdmin() {
             <tbody key={index} className="order-table">
               <tr>
                 <td>{order.customer_name}</td>
-                <td>{order.time}</td>
+                <td>{formatDate(order.time)}</td>
                 <td>{order.type}</td>
                 <td>{order.total}</td>
               </tr>
